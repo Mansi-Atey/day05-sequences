5532dc2cea05cf20774dc7f56eaa3372e713217c
unknown <you@example.com>
2021-02-19 18:22:40 +0530

    Added: sum and avg of random numbers

--- sequence.sh
+++ sequence.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-randomNumber=$((($RANDOM%6)+1))
-sum=$(($randomNumber + $randomNumber))
-echo "sum is :-" $sum
+sum=0
+avg=0
+n=5
+for ((i=0;i<5;i++))
+ do
+    randomNumber=$((10 + $RANDOM%99))
+  echo $randomNumber
+sum=$(($randomNumber))
+done
+echo "sum is:-" $sum
+avg=$(($sum/n))
+echo "average is :-" $avg
+
 
