a372ac12ceae044619571a0dbb3dcc134b1cb57b
unknown <you@example.com>
2021-02-19 18:25:57 +0530

    Added: code of unit conversion

--- sequence.sh
+++ sequence.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-sum=0
-avg=0
-n=5
-for ((i=0;i<5;i++))
- do
-    randomNumber=$((10 + $RANDOM%99))
-  echo $randomNumber
-sum=$(($randomNumber))
-done
-echo "sum is:-" $sum
-avg=$(($sum/n))
-echo "average is :-" $avg
+feet=42
+inch=$(($feet*12))
+echo "Total inches are :-" $inch
 
 
